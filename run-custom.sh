e0bd4db88bdcc01c0643953ff8c92036dc6b73b2
nidheeshdas thavorath <nidheeshdas@gmail.com>
2015-10-29 12:36:14 +0530

    fixes copying ssh keys

--- run-custom.sh
+++ run-custom.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cat $DEP_PRI_KEY > ~/.ssh/id_rsa
-cat $DEP_PUB_KEY > ~/.ssh/id_rsa.pub
+mkdir ~/.ssh
+echo $DEP_PRI_KEY > ~/.ssh/id_rsa
+echo $DEP_PUB_KEY > ~/.ssh/id_rsa.pub
 cd /app && git clone git@bitbucket.org:globemetrix/wp-app.git
 
 /./run.sh
