140eed2a1d1d5ff376d05e93d55820afac32b650
nidheeshdas thavorath <nidheeshdas@gmail.com>
2015-10-29 15:11:45 +0530

    protect keys for use with ssh

--- run-custom.sh
+++ run-custom.sh
@@ -3,6 +3,7 @@
 mkdir ~/.ssh
 echo $DEP_PRI_KEY > ~/.ssh/id_rsa
 echo $DEP_PUB_KEY > ~/.ssh/id_rsa.pub
+chmod -R 600 ~/.ssh
 cd /app && git clone git@bitbucket.org:globemetrix/wp-app.git
 
 /./run.sh
